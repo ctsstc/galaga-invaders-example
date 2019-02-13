71e97d224c1924de983097334042743a8666e78f
Cody Swartz <ctsstc@gmail.com>
2019-02-13 11:09:35 -0800

    Answer questions

--- ship.rb
+++ ship.rb
@@ -43,8 +43,21 @@ end
 Reflecting on Principle
 -----------------------
 1. What does the OOD promise to give us, as programmers?
+  - A smoother tomorrow, where we can implement new features without fear of a big spaghetti mess.
+  - OOD is about managing dependencies.
 2. What does Martin's SOLID acronym stand for? (Just expand the acronym.)
+  - Single Responsibility
+  - Open-Closed
+  - Liskov Substitution
+  - Interface Segregation
+  - Dependency Inversion
 3. True or False: It is our professional responsibility to always spend time designing before writing our code (designing now rather than later).
+  - True
 4. In Metz' TRUE acronym, what does the E stand for and what does it mean?
+  - Exemplary: the code itself should encourage future developers how change it to continue the TRUE qualities.
 5. How does applying SRP naturally lead to good design?
+  - Something like a class will do only what it needs to do
+    - This results in smaller code for that class that is more easily digestible.
+  - If a class has more than one responsibility it is hard to reuse.
+  - Tightly coupled responsibilities that do too much can lead to duplicate code because you're not able to use only what you need
 =end
